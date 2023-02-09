b24a8fca030ef512db568c27c976c2ade085b393
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-09 07:16:00 -0700

    Revert "Add @docupotamus/docusaurus-styles-classic theme customCss for index."
    This reverts commit 88c6690e4297a08afe27751176aa622fd0a92c95.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import type {
     LoadContext,
     PluginConfig,
-    PluginOptions,
-    Preset
+    PluginOptions, Preset
 } from '@docusaurus/types';
 import type { Options, ThemeConfig } from './options';
 
@@ -37,23 +36,7 @@ export default function preset(
     } = opts;
 
     const themes: PluginConfig[] = [];
-    const customCssClassic = require.resolve('@docupotamus/docusaurus-styles-classic');
-    let themeResolved = theme;
-    if (!themeResolved) {
-        themeResolved = {
-            customCss: [],
-        };
-    }
-    if (themeResolved.customCss === undefined) {
-        themeResolved.customCss = customCssClassic;
-    }
-    if (typeof themeResolved.customCss === 'string') {
-        themeResolved.customCss = [themeResolved.customCss, customCssClassic];
-    } else {
-        themeResolved.customCss.push(customCssClassic);
-    }
-    console.log(themeResolved);
-    themes.push(makePluginConfig('@docusaurus/theme-classic', themeResolved));
+    themes.push(makePluginConfig('@docusaurus/theme-classic', theme));
     if (algolia) {
         themes.push(require.resolve('@docusaurus/theme-search-algolia'));
     }
