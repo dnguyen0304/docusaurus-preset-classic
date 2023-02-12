a479476daafe9406e052d33549ff2668cd522df7
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-11 17:27:29 -0700

    Add wrappers for plugin theme components.

--- src/plugin/theme/DocSidebar/Desktop/Content/index.tsx
+++ src/plugin/theme/DocSidebar/Desktop/Content/index.tsx
@@ -1,2 +1,15 @@
-// TODO(dnguyen0304): Fix missing type declaration.
-export { DocSidebarDesktopContent as default } from '@theme/docupotamus-command-menu/package';
+import type { WrapperProps } from '@docusaurus/types';
+import ContentInit from '@theme-init/DocSidebar/Desktop/Content';
+import type ContentType from '@theme/DocSidebar/Desktop/Content';
+import { DocSidebarDesktopContentDecorator as CommandMenuDecorator } from '@theme/docupotamus-command-menu'; // TODO(dnguyen0304): Fix missing type declaration.
+import * as React from 'react';
+
+type Props = Readonly<WrapperProps<typeof ContentType>>;
+
+export default function ContentWrapper(props: Props): JSX.Element {
+    return (
+        <CommandMenuDecorator>
+            <ContentInit {...props} />
+        </CommandMenuDecorator>
+    );
+};
--- src/plugin/theme/Root/index.tsx
+++ src/plugin/theme/Root/index.tsx
@@ -1,2 +1,15 @@
-// TODO(dnguyen0304): Fix missing type declaration.
-export { Root as default } from '@theme/docupotamus-command-menu/package';
+import type { WrapperProps } from '@docusaurus/types';
+import RootInit from '@theme-init/Root';
+import { RootDecorator as CommandMenuDecorator } from '@theme/docupotamus-command-menu'; // TODO(dnguyen0304): Fix missing type declaration.
+import type RootType from '@theme/Root';
+import * as React from 'react';
+
+type Props = Readonly<WrapperProps<typeof RootType>>;
+
+export default function RootWrapper(props: Props): JSX.Element {
+    return (
+        <CommandMenuDecorator>
+            <RootInit {...props} />
+        </CommandMenuDecorator>
+    );
+};
